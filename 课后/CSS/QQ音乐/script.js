af033914cdcc8a6215a6d260b9fb9d7310234f37
hxm193514 <1935197914@qq.com>
2018-12-06 12:10:45 +0800

    QQ音乐4.0

--- 课后/CSS/QQ音乐/script.js
+++ 课后/CSS/QQ音乐/script.js
@@ -1,24 +1,46 @@
+// function animate(obj, target) {
+//     clearInterval(obj.timer);  // 先清除定时器
+//     var speed = obj.offsetLeft < target ? 15 : -15;  // 用来判断 应该 +  还是 -
+//     obj.timer = setInterval(function () {
+//         var result = target - obj.offsetLeft; // 因为他们的差值不会超过15
+//         obj.style.left = obj.offsetLeft + speed + "px";
+//         if (Math.abs(result) <= 15)  // 如果差值不小于 15 说明到位置了
+//         {
+//             clearInterval(obj.timer);
+//             obj.style.left = target + "px";  // 有5像素差距   我们直接跳转目标位置
+//         }
+//     }, 10)
+// }
 window.onload = function () {
-    var slideshow_li = document.querySelectorAll(".slideshow li").length / 5; //算出有几页图片
+    var ulLis ="wntj";
+    var slideshow_li = ulLis.length / 5; //算出有几页图片
     var top_nav = document.getElementById("top-nav").getElementsByTagName("li");
     var top_subnav = document.getElementById("top_subnav").getElementsByTagName("li");
     var top_nava = document.querySelectorAll(".top-nav li a");
     var top_subnava = document.querySelectorAll(".top_subnav li a");
     var img = document.querySelectorAll(".lunbo");
     var span = document.querySelectorAll(".lunbo > span");
-    //头部按钮
+var trmer = null;
 
+  var index = 0;
+
+    //头部按钮
+    for(var i=0;i<5;i++)
+    {
+        $(".slideshow")[0].appendChild($(".slideshow")[0].children[i].cloneNode(true));
+    }
+   
     for (var i = 0; i < top_nav.length; i++) {
         (function (n) {
             top_nav[i].onclick = function () {
                 for (var j = 0; j < top_nav.length; j++) {
                     top_nav[j].className = 'top-nav-z';
                     top_nava[j].className = ""
-                    console.log(top_nav[j]);
+                   
                 }
                 this.className += " current";
                 top_nava[n].className = " acurrent"
-                console.log(top_nav[n]);
+               
             }
         })(i);
     }
@@ -53,63 +75,90 @@ window.onload = function () {
                     $(".mod_index_tab a")[j].classList.remove("currenta");
                 }
                 $(".mod_index_tab a")[n].className += " currenta";
+                 //轮播图锚点切换
                 switch(n){
-                    case 0:   mm("wntj","gfgd","qg","wlgq","jd","ktvrg");break;
-                    case 1: mm("gfgd","qg","wlgq","jd","ktvrg","wntj"); break;
-                    case 2:mm("qg","wlgq","jd","ktvrg","wntj","gfgd"); break;
-                    case 3: mm("wlgq","jd","ktvrg","wntj","gfgd","qg"); break;
-                    case 4:  mm("jd","ktvrg","wntj","gfgd","qg","wlgq"); break;
-                    case 5:mm("ktvrg","wntj","gfgd","qg","wlgq","jd"); break;
+                    case 0: mm("wntj","gfgd","qg","wlgq","jd","ktvrg");ulLis = "wntj";break;
+                    case 1: mm("gfgd","qg","wlgq","jd","ktvrg","wntj");ulLis = "gfgd"; break;
+                    case 2:mm("qg","wlgq","jd","ktvrg","wntj","gfgd"); ulLis = "qg";break;
+                    case 3: mm("wlgq","jd","ktvrg","wntj","gfgd","qg"); ulLis = "wlgq";break;
+                    case 4:  mm("jd","ktvrg","wntj","gfgd","qg","wlgq"); ulLis = "jd";break;
+                    case 5:mm("ktvrg","wntj","gfgd","qg","wlgq","jd"); ulLis = "ktvrg";break;
                 }
             }
            
         })(i);
     }
-        //轮播图锚点切换
 
        
-        // mm("wntj","gfgd","qg","wlgq","jd","ktvrg");
-        // mm("gfgd","qg","wlgq","jd","ktvrg","wntj");
-        // mm("qg","wlgq","jd","ktvrg","wntj","gfgd");
-        // mm("wlgq","jd","ktvrg","wntj","gfgd","qg");
-        // mm("jd","ktvrg","wntj","gfgd","qg","wlgq");
-        // mm("ktvrg","wntj","gfgd","qg","wlgq","jd");
-
-
-
-
    //轮播图按钮过场动画
     var target = 0;
     var leader = 0;
     $(".mod_index")[0].onmouseover = function () {
-        $(".btn-l")[0].className = "slideshow_btn btn-l btn1"
-        $(".btn-r")[0].className = "slideshow_btn btn-r btn2"
-
+        clearInterval(timer);
+        $(".btn-l")[0].className = "slideshow_btn btn-l btn1";
+        $(".btn-r")[0].className = "slideshow_btn btn-r btn2";
     }
     $(".mod_index")[0].onmouseout = function () {
-
-        $(".btn-l")[0].className = "slideshow_btn btn-l btn3"
-        $(".btn-r")[0].className = "slideshow_btn btn-r btn4"
+        timer = setInterval(autoplay, 4000);
+        $(".btn-l")[0].className = "slideshow_btn btn-l btn3";
+        $(".btn-r")[0].className = "slideshow_btn btn-r btn4";
     }
-    $(".btn-l")[0].onclick = function () {
-        target += 1200;
-        console.log(leader);
-    }
-    $(".btn-r")[0].onclick = function () {
-        target -= 1200;
-        console.log(leader);
-    }
-
-    setInterval(function () {
-        if (target >= 0) {
-            target = 0;
-        } else if (target <= -slideshow_li * 1200) {
-            target = -1200;
+  
+      
+    
+      
+        $(".btn-l")[0].onclick = function () {
+            clearInterval(timer);
+            index--;
+            console.log(index);
+             if (index > $("."+ulLis).length/5-1)  // 后判断
+         {
+            $(".slideshow")[0].style.left = 0;  // 迅速调回
+            index = 1;  // 因为第6张就是第一张  第6张播放 下次播放第2张
+              
+         }
+        
+         action($(".slideshow")[0], -index * 1200);
         }
-        leader = leader + (target - leader) / 10;
-        $(".slideshow")[0].style.left = leader + "px";
+        $(".btn-r")[0].onclick = function () {
+            clearInterval(timer);
+            index++;
+            console.log(index);
+            if (index > $("."+ulLis).length/5-1)  // 后判断
+            {
+                $(".slideshow")[0].style.left = 0;  // 迅速调回
+                index = 1;  // 因为第6张就是第一张  第6张播放 下次播放第2张
+            }
+           
+                action($(".slideshow")[0], -index * 1200);
+            
+        }
+ 
+     
+    //轮播图转动
+     function  action(obj, target,) {
+        clearInterval(obj.timer);  // 先清除定时器
+       
+        // if (cerindex > $("."+ulLis).length/5-1)  // 后判断
+        // {
+        //     $(".slideshow")[0].style.left = 0;  // 迅速调回
+        //     cerindex = 0;  // 因为第6张就是第一张  第6张播放 下次播放第2张
+        // }
+        // else{
+            var speed = obj.offsetLeft < target ? 15 : -15;  // 用来判断 应该 +  还是 -
+            obj.timer = setInterval(function () {
+                var result = target - obj.offsetLeft; // 因为他们的差值不会超过15
+                obj.style.left = obj.offsetLeft + speed + "px";
+                if (Math.abs(result) <= 15)  // 如果差值不小于 15 说明到位置了
+                {
+                    clearInterval(obj.timer);
+                    obj.style.left = target + "px";  // 有5像素差距   我们直接跳转目标位置
+                }
+            }, 10)
+        // }
+    
+     }
 
-    }, 30)
   //轮播图播放器过场动画
     for (var i = 0; i < img.length; i++) {
         (function (n) {
@@ -118,7 +167,6 @@ window.onload = function () {
                     img[j].className = "";
                     span[j].className = "";
                 }
-                console.log("1");
                 img[n].className += " simg";
                 span[n].className += "play play1";
             }
@@ -127,13 +175,26 @@ window.onload = function () {
                     img[j].className = "";
                     span[j].className = "";
                 }
-                console.log("1");
                 img[n].className += " simg1";
                 span[n].className += "play play2";
 
             }
 
         })(i);
+    }
+    timer = setInterval(autoplay, 4000); 
+    
+    function autoplay() {
+        index++;  // 先 ++
+        if (index > $("."+ulLis).length/5- 1)  // 后判断
+        {
+            $(".slideshow")[0].style.left = 0;  // 迅速调回
+            index = 1;  // 因为第6张就是第一张  第6张播放 下次播放第2张
+        }
+        console.log(ulLis);
+        action($(".slideshow")[0], -index * 1200);  // 再执行
+       
+        // 小方块
     }
 
 
